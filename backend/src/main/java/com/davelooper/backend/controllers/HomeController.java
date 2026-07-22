d815a709015a691195f1b4499a0bd036a47c6d71
Davelooper <ddonnot@gmail.com>
2026-07-22 14:33:49 +0200

    feat(logging): add application startup, request, and smoke test logs

--- backend/src/main/java/com/davelooper/backend/controllers/HomeController.java
+++ backend/src/main/java/com/davelooper/backend/controllers/HomeController.java
@@ -1,12 +1,15 @@
 package com.davelooper.backend.controllers;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class HomeController {
   @GetMapping("/")
   public String home() {
+    log.info("Smoke test succeeded");
     return "Bienvenue dans l'application de recettes !";
   }
 }
